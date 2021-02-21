f2e5dfe802a3e3bce5b0e25f75fab9987128a691
Reey <trommxxx@mail.ru>
2021-02-21 22:11:13 +0600

    Add result.html

--- hw_44/webapp/views.py
+++ hw_44/webapp/views.py
@@ -9,10 +9,6 @@ def index_view(request):
     if request.method == 'GET':
         return render(request, 'index.html')
     elif request.method == 'POST':
-        errors = {
-            'error1':
-        }
-
         context = {
             'number1': request.POST.get('number1'),
             'number2': request.POST.get('number2'),
@@ -20,27 +16,21 @@ def index_view(request):
             'number4': request.POST.get('number4'),
 
         }
-        try:
-            numbers = [context['number1'], context['number2'], context['number3'], context['number4']]
-            need_check = validation(numbers)
-            if not need_check:
-                if len(numbers) == len(secret_nums):
-                    while idx < 4:
-                        if secret_nums[idx] == numbers[idx]:
-                            bulls += 1
-                        elif numbers[idx] in secret_nums:
-                            cows += 1
-                        idx += 1
-                    response_body += f'<div>Bulls: {bulls}, Cows: {cows}</div>'
-                else:
-                    response_body += '<div>The number of digits is not the same</div>'
-            else:
-                response_body += need_check
-        except ValueError:
-            response_body += "<div>It's not a number</div>"
-        context = {
-            'title': request.POST.get('numbers'),
-        }
+        numbers = [context['number1'], context['number2'], context['number3'], context['number4']]
+        need_check = validation(numbers)
+        if not need_check:
+            if len(numbers) == len(secret_nums):
+                while idx < 4:
+                    if secret_nums[idx] == numbers[idx]:
+                        bulls += 1
+                    elif numbers[idx] in secret_nums:
+                        cows += 1
+                    idx += 1
+                context['bulls'] = bulls
+                context['cows'] = cows
+        else:
+            context["error"] = need_check
+            return render(request, 'errors.html', context)
 
         return render(request, 'article_view.html', context)
 
